7a8bfdfff158903cc488b53f4c78c9a1c3f5df4b
Edward Bayona <edward.bayona@gmail.com>
2023-06-09 08:05:50 -0500

    se anexo condicional1

---
+++ src/EjemploCondicionales.java
@@ -0,0 +1,16 @@
+
+public class EjemploCondicionales {
+	public static void main(String[] args) {
+		System.out.println("hola");
+		//condicionales
+		
+		int edad =28;
+		
+		
+		if (edad >= 18){
+			
+			System.out.println("Es mayor de edad");
+		}
+		
+	}
+}
